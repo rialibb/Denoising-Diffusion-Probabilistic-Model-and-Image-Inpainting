8e2f4a7177f4f92ab97ec86809ed7837514978a3
Ibrahim <ibrahimalkhalilridene.prepa@gmail.com>
2024-12-15 17:51:05 +0100

    MODIF

--- main.py
+++ main.py
@@ -21,27 +21,27 @@ if __name__ == "__main__":
     #    beta_max = 0.02
     #)
 
-    run_scheduler_tuning_pipeline(
-        T = 1000,
-        dataset_choice = "MNIST",   # "MNIST", "Fashion" ,  "CIFAR" or "CelebA"
-        n_epochs = 20,
-        batch_size = 128,
-        lr = 0.001,
-        beta_min = 0.0001,
-        beta_max = 0.02
-    )
-    
-    #run_hyperparam_tuning_pipeline(
+    #run_scheduler_tuning_pipeline(
+    #    T = 1000,
     #    dataset_choice = "MNIST",   # "MNIST", "Fashion" ,  "CIFAR" or "CelebA"
-    #    n_epochs = 15,
+    #    n_epochs = 20,
     #    batch_size = 128,
-    #    num_trials = 5,
     #    lr = 0.001,
-    #    scheduler = "linear",  # "linear", "cosine", "quadratic", "exponential", "logarithmic"
+    #    beta_min = 0.0001,
+    #    beta_max = 0.02
     #)
+    
+    run_hyperparam_tuning_pipeline(
+        dataset_choice = "CIFAR",   # "MNIST", "Fashion" ,  "CIFAR" or "CelebA"
+        n_epochs = 15,
+        batch_size = 128,
+        num_trials = 5,
+        lr = 0.001,
+        scheduler = "linear",  # "linear", "cosine", "quadratic", "exponential", "logarithmic"
+    )
 
     run_sampling_and_inpainting_pipeline(
-        dataset_choice = "MNIST",   # "MNIST", "Fashion" ,  "CIFAR" or "CelebA"
+        dataset_choice = "CIFAR",   # "MNIST", "Fashion" ,  "CIFAR" or "CelebA"
         batch_size = 128,
         scheduler = "linear",  # "linear", "cosine", "quadratic", "exponential", "logarithmic"
     )
